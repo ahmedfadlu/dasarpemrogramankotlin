a74d1737a6d9b8aaf62f98b35ce4964cc8d301e6
Ahmed Fadlu <ahmedfadlu28@gmail.com>
2024-09-03 12:32:16 +0700

    Add variabel2.kt

---
+++ src/variabel2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic varibel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static varibel =" + staticVar )
+}
